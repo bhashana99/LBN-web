6039fc51f30146caec0e8576045b9c12c8fffff4
Bhashana Chamodya <bhashanachamodya99@gmail.com>
2023-12-24 12:53:03 +0530

    complete header component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,7 @@
 import {BrowserRouter,Routes,Route } from 'react-router-dom';
 import Home from './pages/Home';
-import AboutUs from './pages/AboutUs';
-import ContactUs from './pages/ContactUs';
+import About from './pages/About';
+import Contact from './pages/Contact';
 import Header from './components/Header';
 import Jobs from './pages/Jobs';
 
@@ -11,8 +11,8 @@ export default function App() {
     <Header/>
       <Routes>
         <Route path='/' element={ <Home/> } />
-        <Route path='/about-us' element={ <AboutUs/> } />
-        <Route path='/contact-us' element={ <ContactUs/> } />
+        <Route path='/about' element={ <About/> } />
+        <Route path='/contact' element={ <Contact/> } />
         <Route path='/jobs' element={ <Jobs/> } />
       </Routes>
     </BrowserRouter>
--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,35 +1,57 @@
 import { Link } from "react-router-dom";
+import { useState } from "react";
+import img1 from "./../assets/LBN.png";
+import {AiOutlineClose,AiOutlineMenu} from 'react-icons/ai'
 
 export default function Header() {
-  return (
-    <header className="bg-slate-200 shadow-lg">
-      <div className="flex justify-between items-center max-w-6xl mx-auto p-7">
-        <Link to="/">
-          <h1 className="font-bold text-sm sm:text-xl">
-            <span className="text-red-400"> Logo</span>
-          </h1>
-        </Link>
+  const [showMenu, setShowMenu] = useState(true);
 
-        <ul className="flex gap-12">
-          <Link to="/">
-            <li className="hidden sm:inline text-slate-700 hover:underline">
-              HOME
-            </li>
-          </Link>
-          <Link to="/about-us">
-            <li className="hidden sm:inline text-slate-700 hover:underline">
-              ABOUT US
-            </li>
-          </Link>
-          <Link to="/contact-us">
-            <li className="hidden sm:inline text-slate-700 hover:underline">
-              CONTACT US
-            </li>
-          </Link>
-          <Link to="/jobs">
-            <li className="sm:inline text-slate-700 hover:underline">JOBS</li>
-          </Link>
+  const handleMenu = () =>{
+    setShowMenu(!showMenu);
+  }
+
+  const handleClick = () =>{
+    setShowMenu(!showMenu)
+  }
+  return (
+    <header className="bg-slate-100 shadow-lg sticky">
+      <div className="flex justify-between items-center max-w-7xl mx-auto p-4">
+        <Link to="/" className="z-20">
+          <div className="  flex items-center gap-3">
+            <div>
+              <img src={img1} alt="LBN LOGO" className="w-10 h-10" />
+            </div>
+            <div className="text-sm sm:text-xl font-bold">
+              <h1 className="flex flex-col">
+                <span className="text-red-400 companyName ">
+                  LBN KEY RESOURCE (PVT) LTD
+                </span>
+                <span className="companyH1 text-sm flex justify-end">
+                  The Head Hunters
+                </span>
+              </h1>
+            </div>
+          </div>
+        </Link>
+        <ul className="hidden md:flex gap-4 min-w-3xl uppercase headerList">
+          <Link  to="/"><li>Home</li></Link>
+          <Link to='/jobs'><li>Jobs</li></Link>
+          <Link  to='/about'><li>About</li></Link>
+          <Link to='/contact'><li >Contact</li></Link>
         </ul>
+        <div onClick={handleMenu} className="block md:hidden">
+          {!showMenu ? <AiOutlineClose size={30}  /> : <AiOutlineMenu size={30}/> }
+        </div>
+        
+        <div className={!showMenu ? 'fixed left-0 top-0 w-[80%] h-full border-r border-r-gray-900  bg-slate-200 ease-in-out duration-500 ' : 'fixed left-[-100%] '}>
+          
+          <ul className="pt-24 uppercase headerList">
+          <Link to='/' onClick={handleClick}><li className="p-3 border-b border-slate-50">Home</li></Link>
+          <Link to='jobs' onClick={handleClick}><li className="p-3 border-b border-slate-50">Jobs</li></Link>
+          <Link to='about' onClick={handleClick}><li className="p-3 border-b border-slate-50">About</li></Link>
+          <Link to='contact' onClick={handleClick}><li className="p-3 border-b border-slate-50">Contact</li></Link>
+          </ul>
+        </div>
       </div>
     </header>
   );
