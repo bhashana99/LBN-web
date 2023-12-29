59563ae40577bb69a9e56228bdd241280975c77a
Bhashana Chamodya <bhashanachamodya99@gmail.com>
2023-12-29 17:27:28 +0530

    add pre installation

--- api/index.js
+++ api/index.js
@@ -1,7 +1,16 @@
 import express from 'express';
+import path from 'path';
+
+const __dirname = path.resolve();
 
 const app = express();
 
 app.listen(3000, ()=>{
     console.log('Server is running port 3000');
 })
+
+app.use(express.static(path.join(__dirname, "/client/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client","dist", "index.html"));
+});
