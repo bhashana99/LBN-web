d820689fb5b5519867fb028af0665191b9b1df1f
Bhashana Chamodya <bhashanachamodya99@gmail.com>
2023-12-29 17:40:02 +0530

    cancel deploy

--- api/index.js
+++ api/index.js
@@ -1,7 +1,4 @@
 import express from 'express';
-import path from 'path';
-
-const __dirname = path.resolve();
 
 const app = express();
 
@@ -9,8 +6,3 @@ app.listen(3000, ()=>{
     console.log('Server is running port 3000');
 })
 
-app.use(express.static(path.join(__dirname, "/client/dist")));
-
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "client","dist", "index.html"));
-});
