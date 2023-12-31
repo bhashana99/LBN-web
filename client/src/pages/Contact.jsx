4dfff9769e1a881bc6b7032386c619ac16247922
Bhashana Chamodya <bhashanachamodya99@gmail.com>
2023-12-31 11:08:41 +0530

    complete contact page

--- client/src/pages/Contact.jsx
+++ client/src/pages/Contact.jsx
@@ -1,10 +1,18 @@
 import React from "react";
 import FadeIn from "../components/FadeIn";
 import contactImg from "../assets/images/contactImg.png";
-import { FaHome, FaFax,FaWhatsapp } from "react-icons/fa";
+import { Link } from "react-router-dom";
+import {
+  FaHome,
+  FaFax,
+  FaWhatsapp,
+  FaFacebookSquare,
+  FaLinkedin,
+  FaInstagram,
+} from "react-icons/fa";
 import { IoIosCall, IoIosMail } from "react-icons/io";
 import { CiClock2 } from "react-icons/ci";
-
+import { FaSquareXTwitter } from "react-icons/fa6";
 export default function Contact() {
   return (
     <div className="max-w-6xl mx-auto w-full  poppins mt-5 md:mt-10 ">
@@ -35,7 +43,7 @@ export default function Contact() {
                   LBN KEY RESOURCE (PVT) LTD,
                   <br />
                 </span>
-                NO.250D/2, PANNIPITIYA ROAD, THALAWATHUGODA, <br />
+                NO: 250 D/2, PANNIPITIYA ROAD, THALAWATHUGODA, <br />
                 SRI LANKA.
               </p>
             </div>
@@ -61,9 +69,11 @@ export default function Contact() {
             </div>
 
             <div>
-              <p>Send a text message on whatsapp<br />
-                <span className="ml-5">+94 77 350 1689</span></p>
-              
+              <p>
+                Send a text message on whatsapp
+                <br />
+                <span className="ml-5">+94 77 350 1689</span>
+              </p>
             </div>
           </div>
           <hr className="bg-gray-100 border-0 rounded h-1 my-2" />
@@ -88,7 +98,8 @@ export default function Contact() {
             <div>
               <p>
                 Send us email for any Information <br />
-                <span className="ml-5">info@lbnkeyresource.com</span><br />
+                <span className="ml-5">info@lbnkeyresource.com</span>
+                <br />
                 <span className="ml-5">lbnkeyresource99@gmail.com</span>
               </p>
             </div>
@@ -101,7 +112,8 @@ export default function Contact() {
 
             <div>
               <p>
-              Office Hours<br />
+                Office Hours
+                <br />
                 <span className="ml-5">9.00 am - 4.00 pm</span>
               </p>
             </div>
@@ -110,10 +122,66 @@ export default function Contact() {
         </div>
       </div>
 
-    
+      {/* social media links */}
+      <div className="bg-slate-500 p-5 my-10">
+        <FadeIn delay={0.4} direction="middle" padding fullWidth>
+          <h3 className="text-center text-2xl font-semibold">
+            Our Social Media Platforms
+          </h3>
+          <hr className="w-48 h-1 mx-auto my-2 bg-gray-100 border-0 rounded  dark:bg-gray-700" />
+
+          <div className="flex flex-row justify-center gap-5 mt-5">
+            <div>
+              <Link
+                to="https://www.facebook.com/LBNKEYRESOURCEPVTLTD?mibextid=ZbWKwL "
+                target="_blank"
+              >
+                <FaFacebookSquare className="text-4xl text-blue-600 mr-2 bg-white hover:scale-110" />
+              </Link>
+            </div>
+            <div>
+              <Link to="" target="_blank">
+                <FaLinkedin className="text-4xl text-blue-800 mr-2 bg-white hover:scale-110" />
+              </Link>
+            </div>
+            <div>
+              <Link to="" target="_blank">
+                <FaSquareXTwitter className="text-4xl text-black mr-2 bg-white hover:scale-110" />
+              </Link>
+            </div>
+            <div>
+              <Link to="" target="_blank">
+                <FaInstagram className="text-4xl text-rose-600 mr-2 bg-white hover:scale-110" />
+              </Link>
+            </div>
+          </div>
+        </FadeIn>
+      </div>
 
       {/* location */}
-      <div></div>
+      <FadeIn delay={0.2} direction="middle" padding fullWidth>
+        <div className="flex flex-col md:flex-row gap-10 justify-center my-10 md:my-32 items-center bg-slate-200 p-10">
+          <div className="text-4xl">
+            <h2 className="font-bold text-rose-500">Our Location</h2>
+          </div>
+          <div>
+            <iframe
+            
+              width="520"
+              height="400"
+              frameborder="0"
+              scrolling="no"
+              marginheight="0"
+              marginwidth="0"
+              src="https://maps.google.com/maps?width=520&amp;height=400&amp;hl=en&amp;q=%20250%20D/2%20Battaramulla%20-%20Pannipitiya%20Rd,Sri%20Jayawardenepura,Kotte,Srilanka+(LBN%20KEY%20RESOURCE%20(PVT)%20LTD)&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"
+            >
+              <a href="https://www.maps.ie/population/">
+                Population Estimator map
+              </a>
+            </iframe>
+          </div>
+        </div>
+      </FadeIn>
     </div>
   );
 }
