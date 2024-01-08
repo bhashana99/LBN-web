bb9425a527885194fc49a0a0afca831ca401f45d
Bhashana Chamodya <bhashanachamodya99@gmail.com>
2024-01-08 16:02:13 +0530

    create admin login api route

--- api/controllers/admin.controller.js
+++ api/controllers/admin.controller.js
@@ -1,5 +1,7 @@
 import Admin from "../models/admin.model.js";
 import bcryptjs from "bcryptjs";
+import jwt from "jsonwebtoken";
+import { errorHandler } from "../utils/error.js";
 
 export const createAdmin = async (req, res, next) => {
   const { username, password } = req.body;
@@ -9,6 +11,28 @@ export const createAdmin = async (req, res, next) => {
   try {
     await newAdmin.save();
     res.status(201).json("Admin created successfully!!");
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const loginAdmin = async (req, res, next) => {
+  const { username, password } = req.body;
+
+  try {
+    const validUser = await Admin.findOne({ username });
+    if (!validUser) return await next(errorHandler(404, "User not found!!"));
+    const validPassword = bcryptjs.compareSync(password, validUser.password);
+    if (!validPassword) return next(errorHandler(401, "Wrong credential!"));
+    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+    const { password: pass, ...rest } = validUser._doc;
+
+    res
+      .cookie("access_token", token, {
+        httpOnly: true,
+      })
+      .status(200)
+      .json(validUser);
   } catch (error) {
     next(error);
   }
--- api/routes/admin.route.js
+++ api/routes/admin.route.js
@@ -1,9 +1,10 @@
 import express from 'express';
-import {createAdmin} from '../controllers/admin.controller.js';
+import {createAdmin,loginAdmin} from '../controllers/admin.controller.js';
 
 
 const router = express.Router();
 
 router.post('/create-admin',createAdmin);
+router.post('/login-admin',loginAdmin);
 
 export default router;
