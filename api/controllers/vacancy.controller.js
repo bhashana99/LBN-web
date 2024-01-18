7866237d73e22c81bf2b065b000e78396f85fdbf
Bhashana Chamodya <bhashanachamodya99@gmail.com>
2024-01-18 09:23:35 +0530

    update vacancy route

--- api/controllers/vacancy.controller.js
+++ api/controllers/vacancy.controller.js
@@ -1,38 +1,54 @@
-import {errorHandler} from "../utils/error.js";
+import { errorHandler } from "../utils/error.js";
 import Vacancy from "../models/vacancy.model.js";
 
 export const createVacancy = async (req, res, next) => {
-    try {
-        const vacancy = await Vacancy.create(req.body);
-        return res.status(201).json(vacancy);
-    } catch (error) {
-        next(error);
-    }
-   
-}
+  try {
+    const vacancy = await Vacancy.create(req.body);
+    return res.status(201).json(vacancy);
+  } catch (error) {
+    next(error);
+  }
+};
 
 export const getVacancies = async (req, res, next) => {
-    try {
-        const vacancies = await Vacancy.find();
-        if(!vacancies) return next(errorHandler(404, "No vacancies found"));
-         res.status(200).json(vacancies);
-    } catch (error) {
-        next(error);
-    }
-}
+  try {
+    const vacancies = await Vacancy.find();
+    if (!vacancies) return next(errorHandler(404, "No vacancies found"));
+    res.status(200).json(vacancies);
+  } catch (error) {
+    next(error);
+  }
+};
 
 export const deleteVacancy = async (req, res, next) => {
-    const vacancy = await Vacancy.findById(req.params.id);
+  const vacancy = await Vacancy.findById(req.params.id);
 
-    if(!vacancy){
-        return next(errorHandler(404, "Vacancy not found"));
-    }
+  if (!vacancy) {
+    return next(errorHandler(404, "Vacancy not found"));
+  }
 
-    try {
-        await Vacancy.findByIdAndDelete(req.params.id);
-        res.status(200).json({message: "Vacancy deleted successfully"});
-    } catch (error) {
-        next(error);
-        
-    }
-}
+  try {
+    await Vacancy.findByIdAndDelete(req.params.id);
+    res.status(200).json({ message: "Vacancy deleted successfully" });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const updateVacancy = async (req, res, next) => {
+  const vacancy = await Vacancy.findById(req.params.id);
+  if (!vacancy) {
+    return next(errorHandler(404, "Vacancy not found"));
+  }
+
+  try {
+    const updateVacancy = await Vacancy.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      { new: true }
+    );
+    res.status(200).json(updateVacancy);
+  } catch (error) {
+    next(error);
+  }
+};
--- api/routes/vacancy.route.js
+++ api/routes/vacancy.route.js
@@ -1,11 +1,12 @@
 import express from 'express';
-import { createVacancy,getVacancies,deleteVacancy } from '../controllers/vacancy.controller.js';
+import { createVacancy,getVacancies,deleteVacancy,updateVacancy } from '../controllers/vacancy.controller.js';
 import { verifyToken } from '../utils/verifyAdmin.js';
 
 const router = express.Router();
 
 router.post('/create-vacancy', verifyToken, createVacancy);
 router.get('/get-vacancies',getVacancies);
-router.delete('/delete-vacancy/:id',deleteVacancy);
+router.delete('/delete-vacancy/:id',verifyToken,deleteVacancy);
+router.post('/update-vacancy/:id',verifyToken,updateVacancy)
 
 export default router;
