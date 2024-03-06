87d6c9401dd5b2c2285c21969679528141ec234a
Bhashana Chamodya <bhashanachamodya99@gmail.com>
2024-03-07 01:00:17 +0530

    vacancies page mobile screen changes

--- client/src/pages/Vacancies.jsx
+++ client/src/pages/Vacancies.jsx
@@ -23,7 +23,6 @@ export default function Vacancies() {
         );
 
         setShowVacancy(sortedData);
-
       } catch (error) {
         setShowVacancyError(true);
         console.log(showVacancyError);
@@ -34,52 +33,56 @@ export default function Vacancies() {
   });
 
   return (
-    <div className=" max-w-4xl  mx-auto w-full my-10">
-      <div>
-        <h1 className="text-3xl font-bold text-rose-700 underline mb-5">
-          Vacancies
-        </h1>
-      </div>
-      <div>
-        {showVacancy && showVacancy.length > 0 && (
-          <div className="">
-            {showVacancy.map((vacancy) => (
-              <div
-                key={vacancy._id}
-                className="flex flex-col md:flex-row  gap-5 border-solid border-2 border-slate-200 mb-4 shadow-2xl md:shadow-lg hover:shadow-2xl mx-5 md:mx-0"
-              >
-                <div className="basis-1/4">
-                  <img
-                    src={vacancy.countryFlag}
-                    alt="country flag"
-                    className="h-40 text-center md:text-start w-full object-cover"
-                  />
-                </div>
-                <div className="flex flex-col gap-3 basis-2/4 ml-3 md:ml-0">
-                  <div className="">
-                    <h3 className="font-semibold mt-2 text-2xl">
-                      {vacancy.title}
-                    </h3>
+    <div className="bg-rose-50">
+      <div className=" max-w-4xl  mx-auto w-full py-10 ">
+        <div>
+          <h1 className="text-xl md:text-3xl ml-5 md:ml-0 font-bold text-rose-700 underline mb-5">
+            Vacancies
+          </h1>
+        </div>
+        <div>
+          {showVacancy && showVacancy.length > 0 && (
+            <div className="">
+              {showVacancy.map((vacancy) => (
+                <div
+                  key={vacancy._id}
+                  className="flex flex-col md:flex-row  gap-5 border-solid border-2 border-slate-200 mb-4 shadow-2xl md:shadow-lg hover:shadow-2xl mx-5 md:mx-0 bg-white"
+                >
+                  <div className="basis-1/4">
+                    <img
+                      src={vacancy.countryFlag}
+                      alt="country flag"
+                      className="h-40 text-center md:text-start w-full object-cover"
+                    />
                   </div>
-                  <div>
-                    <p>{vacancy.description}</p>
+                  <div className="flex flex-col gap-3 basis-2/4 ml-3 md:ml-0">
+                    <div className="">
+                      <h3 className="font-semibold mt-2 text-2xl">
+                        {vacancy.title}
+                      </h3>
+                    </div>
+                    <div>
+                      <p>{vacancy.description}</p>
+                    </div>
+                    <div className="mt-2">
+                      <p className="text-green-800 font-bold">
+                        {vacancy.salary}
+                      </p>
+                    </div>
                   </div>
-                  <div className="mt-2">
-                    <p className="text-green-800 font-bold">{vacancy.salary}</p>
-                  </div>
-                </div>
 
-                <div className="basis-1/4 flex items-center justify-center text-center mb-5 md:mb-0 bg-green-700 md:bg-transparent mx-5 md:mx-0 ">
-                  <Link to={`/vacancy/${vacancy._id}`}>
-                    <button className="bg-green-700 rounded-lg p-4 my-auto text-white">
-                      View More
-                    </button>
-                  </Link>
+                  <div className="basis-1/4 flex items-center justify-center text-center mb-5 md:mb-0 bg-green-700 md:bg-transparent mx-5 md:mx-0 ">
+                    <Link to={`/vacancy/${vacancy._id}`}>
+                      <button className="bg-green-700 rounded-lg p-4 my-auto text-white">
+                        View More
+                      </button>
+                    </Link>
+                  </div>
                 </div>
-              </div>
-            ))}
-          </div>
-        )}
+              ))}
+            </div>
+          )}
+        </div>
       </div>
     </div>
   );
