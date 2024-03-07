79956b5db194912e30c6dee523c96d3a53cff96c
Bhashana Chamodya <bhashanachamodya99@gmail.com>
2024-03-07 10:55:37 +0530

    footer mobile screen design

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -14,11 +14,11 @@ import { MdEmail } from "react-icons/md";
 export default function Footer() {
   return (
     <footer className="bg-red-300 pb-3 md:pb-0 relative left-0 right-0 bottom-0 shadow-md">
-      <div className="flex flex-col mx-5 md:grid  md:grid-cols-8  md:gap-2 md:mx-10  md:p-10">
+      <div className="flex flex-col  mx-5 md:grid  md:grid-cols-8  md:gap-2 md:mx-10  md:p-10">
         {/* logo */}
         <div className=" md:col-span-3 ">
-          <div className="grid grid-cols-3 items-center">
-            <div>
+          <div className="grid-cols-3 items-center hidden md:grid ">
+            <div className="">
               <Link to="/sign-in">
                 <img src={logo} alt="" className="w-28 md:w-40 " />
               </Link>
@@ -37,8 +37,8 @@ export default function Footer() {
           </div>
         </div>
         {/* links */}
-        <div className="md:col-span-2 mt-5">
-          <h1 className="text-2xl font-bold  text-center">Our Links</h1>
+        <div className="md:col-span-2 mt-5 ">
+          <h1 className="text-xl md:text-2xl font-bold  text-center ">Our Links</h1>
           <hr className="w-36 h-1 mx-auto my-1  bg-gray-100 border-0 rounded md:my-1 dark:bg-gray-700" />
           <div className="flex flex-col  mt-2 items-center justify-start">
             <ul className="list-disc">
@@ -62,47 +62,47 @@ export default function Footer() {
         </div>
         {/* contact */}
         <div className="flex flex-col md:col-span-3">
-          <h1 className="text-2xl font-bold text-center">Contact Us</h1>
+          <h1 className="text-xl md:text-2xl font-bold text-center">Contact Us</h1>
           <hr className="w-36 h-1 mx-auto my-1 bg-gray-100 border-0 rounded md:my-1 dark:bg-gray-700" />
           <div className="gird grid-row-2 ">
             <div className="flex flex-col md:grid md:grid-rows-2 md:grid-flow-col gap-1 ">
-              <div className="flex flex-row gap-3 items-center mb-2">
-                <FaMapLocationDot className="text-2xl text-green-800" />
+              <div className="flex flex-row gap-3 items-center mb-2 ">
+                <FaMapLocationDot className="text-lg md:text-3xl text-green-800" />
                 <div>
                   <p>No.250D/2, Pannipitiya Road,Thalawathugoda, </p>
                   <p>Sri Lanka.</p>
                 </div>
               </div>
               <div className="flex flex-row gap-3 ">
-                <BsFillTelephoneFill className="text-xl text-green-800" />
+                <BsFillTelephoneFill className="text-lg md:text-xl text-green-800" />
                 <div>
                   <p>+94 112 796 116</p>
                 </div>
               </div>
-              <div className="flex flex-row gap-3 items-center">
-                <MdEmail className="text-2xl text-green-800" />
-                <div>
+              <div className="flex flex-row gap-3 items-center ">
+                <MdEmail className="text-lg md:text-2xl text-green-800" />
+                <div className="">
                   <p>info@lbnkeyresource.com</p>
                   <p>lbnkeyresource99@gmail.com </p>
                 </div>
               </div>
               <div className="flex flex-row gap-3 ">
-                <FaWhatsapp className="text-2xl text-green-800" />
+                <FaWhatsapp className="text-lg md:text-2xl text-green-800" />
                 <p> +94 773 501 689</p>
               </div>
             </div>
             {/* social media */}
-            <div className=" md:mt-[-40px] flex  ">
+            <div className=" md:mt-[-40px] flex justify-center my-5  ">
               <Link
                 to="https://www.facebook.com/LBNKEYRESOURCEPVTLTD?mibextid=ZbWKwL "
                 target="_blank"
               >
-                <FaFacebookSquare className="text-2xl text-blue-600 mr-2" />
+                <FaFacebookSquare className="text-lg md:text-2xl text-blue-600 mr-2" />
               </Link>
 
-              <FaLinkedin className="text-2xl text-blue-800 mr-2" />
-              <FaSquareXTwitter className="text-2xl text-black mr-2" />
-              <FaInstagram className="text-2xl text-rose-600 mr-2" />
+              <FaLinkedin className="text-lg md:text-2xl text-blue-800 mr-2" />
+              <FaSquareXTwitter className="text-lg md:text-2xl text-black mr-2" />
+              <FaInstagram className="text-lg md:text-2xl text-rose-600 mr-2" />
             </div>
           </div>
         </div>
